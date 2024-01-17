fc80a7b8cd1c6e65988d33a0f9b72a8aa83ce322
NathanChiche <nathanchiche.nc@gmail.com>
2024-01-17 17:37:25 +0100

    bernstein
    range a l'air de marcher sur exemple très simple.
    reste à tester sur exemple difficile + gros nombre de variables

--- bernstein.jl
+++ bernstein.jl
@@ -1,33 +1,85 @@
 
 function createIndexes(maxdeg)
-    indx=ntuple(d->1:maxdeg[1],1)
+    indx=ntuple(d->1:maxdeg[1]+1,1)
     for i in 2:length(maxdeg)
-        indx=(indx...,1:maxdeg[i])
+        indx=(indx...,1:maxdeg[i]+1)
     end
     #return indx
     res=[I for I in Iterators.product(indx...)]
     res=collect(collect(i) for i in res)
     return res
 end
-c
-length(c)
-c[12]
-l::Int=3
-coeffs = Vector(undef, 2)
-coeffs[1]=1;coeffs[2]=1
-am=[2, 1.1]
-coeffs+am
-lll=
-function bernsteinmulti_from_univariates(monomial,maxdeg,domain,indexlist)
+
+
+function monobernsteinmultivar_from_univariates(monomial,maxdeg,domain,indexlist)
+    #on obtient dans la liste coeffs les coeffs de Bernstein multivariés pour un monome précis sur un domaine précis
     ll=multivariate(monomial,maxdeg,domain)
     nbvar=length(maxdeg)
     nb_coeffs=prod(k+1 for k in maxdeg)
     coeffs=Vector{Float64}(undef,nb_coeffs)
+    len=length(indexlist[1])
      for i in 1:length(indexlist)
         coeffs[i]=prod(ll[j][indexlist[i][j]] for j in 1:len)
     end
+    return coeffs
 end
 
+
+function findmaxdeg_listmono(Expo)
+    n1,n2=size(Expo)
+    maxdeg=Vector{Int64}(undef,n2)
+    for i in 1:n1
+        maxdeg[i]=maximum(@view Expo[i,:])
+    end
+
+    @polyvar x[1:n1]
+    listmonomials=Array{Monomial}(undef,n2)
+    for i in 1:n2
+        listmonomials[i]=Monomial(x,Expo[:,i])
+    end
+
+    return maxdeg,listmonomials
+end
+
+
+function every_multivariate_bernsteincoeff(Expo,domain)
+    
+    maxdegr,listmono=findmaxdeg_listmono(Expo)
+    index=createIndexes(maxdegr)
+    println(maxdegr)
+    println(listmono)
+    nbmonomials=size(Expo)[2]
+    All=Array{Vector{Float64}}(undef,nbmonomials)
+    for i in 1:nbmonomials
+        All[i]=monobernsteinmultivar_from_univariates(listmono[i],maxdegr,domain,index)
+    end
+    return All
+end
+
+
+
+
+function ranges_from_Bernsteincoeff(G,E,domain)
+    dim=size(G)[1]
+    coeffs=every_multivariate_bernsteincoeff(E,domain)
+    nbmonomials=length(coeffs)
+    polynomials_coeffs=Vector{Vector{Float64}}(undef,dim)
+    ranges=Vector{Vector{Float64}}(undef,dim)
+
+    for i in 1:dim
+        #for j in 1:length(coeffs)
+        polynomials_coeffs[i]=sum(G[i,j]*coeffs[j] for j in 1:nbmonomials)   
+        ranges[i]=[minimum(polynomials_coeffs[i]),maximum(polynomials_coeffs[i])]
+    end
+    
+    return ranges
+end
+
+G=[1 0; 1 2]
+E=[0 1; 1 1]
+
+ranges_from_Bernsteincoeff(G,E,dom)
+dom
 
 Ma=[0 1; 1 1]
 D=2
@@ -44,24 +96,3 @@ Nt=(Nt...,1:5)
 [I for I in Iterators.product(Nt...)]
 
 
-for i,j in Nt
-    print(i)
-end
-for 
-[1;1]<[2;2]
-
-l=[]
-collect(i for i<5)
-collect([i,j] for i in 1:3 for j in 1:2 if i!=2)
-
-
-
-function createloops(n)
-    if n==0
-        return 
-    end
-    for i=1:n
-        println(i)
-        createloops(n-1)
-    end
-end
